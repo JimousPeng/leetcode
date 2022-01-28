31d5eabe463faac0847bca4d92fb706a2ed3d6be
jimous <jimouspeng@xiaoe-tech.com>
2022-01-28 14:55:23 +0800

    leetcode init

--- src/Array.js
+++ src/Array.js
@@ -2,41 +2,39 @@
  * @Date: 2022-01-28 10:10:45
  * @LastEditors: jimouspeng
  * @Description: 描述文件内容
- * @LastEditTime: 2022-01-28 10:49:33
- * @FilePath: \leetcode\src\index.js
+ * @LastEditTime: 2022-01-28 14:54:46
+ * @FilePath: \leetcode\src\Array.js
  */
 
 /**删除排序数组中的重复项
  * @param {number[]} nums
  * @return {number}
  */
- var removeDuplicates = function(nums) {
-    let left = 0;
-    for(let right = 1; right < nums.length; right ++) {
-        if(nums[right] !== nums[left]) {
+var removeDuplicates = function (nums) {
+    let left = 0
+    for (let right = 1; right < nums.length; right++) {
+        if (nums[right] !== nums[left]) {
             nums[++left] = nums[right]
         }
-    };
+    }
     return ++left
-};
+}
 
 /** 买卖股票的最佳时机 II
  * @param {number[]} prices
  * @return {number}
  */
- var maxProfit = function(prices) {
+var maxProfit = function (prices) {
     // 利润记录
-    let profix = 0;
+    let profix = 0
     // 记录价格锚点
-    let buyPrice = prices[0];
-    for(let i = 1; i< prices.length; i++) {
-        if(prices[i] > buyPrice) {
+    let buyPrice = prices[0]
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > buyPrice) {
             // 只要当前股价不是下跌，那么记录价格点位,并抛出
-            profix += (prices[i] - buyPrice)
+            profix += prices[i] - buyPrice
         }
-        buyPrice = prices[i];
-    };
+        buyPrice = prices[i]
+    }
     return profix
-};
-
-
+}
