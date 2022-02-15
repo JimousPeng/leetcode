ebae96f8abc000df9c77363931e713d76b2956a3
jimous <jimouspeng@xiaoe-tech.com>
2022-02-15 19:05:35 +0800

    fix: 动态规划

--- src/dynamic.js
+++ src/dynamic.js
@@ -2,9 +2,48 @@
  * @Date: 2022-02-15 10:52:04
  * @LastEditors: jimouspeng
  * @Description: 动态规划
- * @LastEditTime: 2022-02-15 13:34:17
+ * @LastEditTime: 2022-02-15 16:18:41
  * @FilePath: \leetcode\src\dynamic.js
  */
+const dynamic = ['1. 确定状态', '  2. 找到转移公式', '  3. 确定初始条件及边界条件', '  4. 计算结果']
+console.log(dynamic.join(' ->'))
+
+/**打家劫舍
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。
+ * 每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+ * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+输入：[1,2,3,1]
+输出：4
+解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+     偷窃到的最高金额 = 1 + 3 = 4 。
+输入：[2,7,9,3,1]
+输出：12
+解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+     偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    // function calculateNum(n, total) {
+    //     if (n < 0) {
+    //         return total
+    //     }
+    //     return calculateNum(n - 1, nums[n] + total)
+    // }
+    const len = nums.length
+    const stoleList = new Array(len)
+    stoleList[0] = nums[0]
+    let maxStole = nums[0]
+    for (let i = 2; i < len; i++) {
+        stoleList[i] = Math.max(maxStole, nums[i])
+    }
+    return maxStole
+}
+let price = [1, 2, 3, 1]
+console.log(rob(price))
 
 /**最大子序和
  * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
@@ -23,10 +62,39 @@
  * @param {number[]} nums
  * @return {number}
  */
- var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
+    // 执行用时： 88 ms , 在所有 JavaScript 提交中击败了 79.13% 的用户
+    // 内存消耗： 52 MB , 在所有 JavaScript 提交中击败了 7.93% 的用户
+    // let maxCount = []
+    // let dp = new Array(nums.length)
+    // dp[0] = nums[0]
+    // let max = dp[0]
+    // for (let i = 1; i < nums.length; i++) {
+    //     // dp[i]用来表示，终点在i的子序列的最佳子序和
+    //     // 如果dp[i-1]大于0，就继续累加，dp[i]=dp[i-1]+num[i]。如果dp[i-1]小于0，我们直接把前面的舍弃，也就是说重新开始计算，否则会越加越小的，直接让dp[i]=num[i]
+    //     dp[i] = Math.max(dp[i - 1], 0) + nums[i]
+    //     // 再比较当前dp[i]是否大于max，如果大于max, 那么重新开始计算
+    //     max = Math.max(max, dp[i])
+    // }
+    // console.log(dp)
+    // return max
 
-};
-
+    // 优化
+    // 执行用时： 76 ms , 在所有 JavaScript 提交中击败了 97.13% 的用户
+    // 内存消耗： 49.2 MB , 在所有 JavaScript 提交中击败了 28.64% 的用户
+    let calculate = nums[0] // 计算差值
+    let max = nums[0]
+    for (let i = 1; i < nums.length; i++) {
+        // dp[i]用来表示，终点在i的子序列的最佳子序和
+        // 如果dp[i-1]大于0，就继续累加，dp[i]=dp[i-1]+num[i]。如果dp[i-1]小于0，我们直接把前面的舍弃，也就是说重新开始计算，否则会越加越小的，直接让dp[i]=num[i]
+        calculate = Math.max(calculate, 0) + nums[i]
+        // 再比较当前dp[i]是否大于max，如果大于max, 那么重新开始计算
+        max = Math.max(max, calculate)
+    }
+    return max
+}
+// let maxlist = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// console.log(maxSubArray(maxlist))
 
 /**买卖股票的最佳时机
  * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
@@ -57,12 +125,12 @@ var maxProfit = function (prices) {
     // }
     // return maxNum
     // 方法2： 一次遍历
-    let minProfit = Infinity;
-    let maxPro = 0;
-    for(let i = 0; i < prices.length; i++) {
-        if(prices[i] < minProfit) {
+    let minProfit = Infinity
+    let maxPro = 0
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minProfit) {
             minProfit = prices[i]
-        } else if(prices[i] - minProfit > maxPro) {
+        } else if (prices[i] - minProfit > maxPro) {
             maxPro = prices[i] - minProfit
         }
     }
