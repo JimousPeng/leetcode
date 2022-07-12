180c899bd30d6bf06a27e554dadbffa9e7e37280
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-07-12 16:23:23 +0800

    fix

--- src/String.js
+++ src/String.js
@@ -2,9 +2,63 @@
  * @Date: 2022-01-28 10:49:52
  * @LastEditors: Please set LastEditors
  * @Description: 字符操作
- * @LastEditTime: 2022-07-11 17:47:08
+ * @LastEditTime: 2022-07-12 16:09:40
  * @FilePath: \leetcode\src\String.js
  */
+
+/** 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。如果不存在公共前缀，返回空字符串 ""
+ * 输入：strs = ["flower","flow","flight"]  输出："fl"
+ */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {};
+
+/** 字符串转换整数 (atoi)
+ * 实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）
+ * 
+ * 函数 myAtoi(string s) 的算法如下：
+
+读入字符串并丢弃无用的前导空格
+检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
+读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
+将前面步骤读入的这些数字转换为整数（即，"123" -> 123， "0032" -> 32）。如果没有读入数字，则整数为 0 。必要时更改符号（从步骤 2 开始）。
+如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
+返回整数作为最终结果。
+注意：
+
+本题中的空白字符只包括空格字符 ' ' 。
+除前导空格或数字后的其余字符串外，请勿忽略 任何其他字符。
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function (s) {
+    /** 执行用时： 120 ms , 在所有 JavaScript 提交中击败了 5.42% 的用户
+     * 内存消耗： 46.3 MB , 在所有 JavaScript 提交中击败了 5.35% 的用户
+     */
+    let strS = s.match(/^[\s-\+]*\d+/g);
+    if (!strS) {
+        return 0;
+    }
+    strS = strS.join('');
+    if (!strS.match(/^\s*[-\+]?\d+/g)) {
+        return 0;
+    }
+    strS = strS.match(/-?\d+/g).join('');
+    const numStr = parseInt(strS);
+    if (numStr < -2147483648) {
+        return -2147483648;
+    }
+    if (numStr > 2147483647) {
+        return 2147483647;
+    }
+    return numStr;
+};
+console.log(myAtoi('   -42'));
 
 /** 验证回文串: 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
  * 说明：本题中，我们将空字符串定义为有效的回文串。
