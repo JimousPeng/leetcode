d44c3e1c3b69ba7c3c53ff076f4b3da9fde06200
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-07-15 18:12:51 +0800

    fix

--- src/math.js
+++ src/math.js
@@ -1,7 +1,82 @@
 /*
  * @Author: jimouspeng
- * @LastEditTime: 2022-03-05 18:11:31
+ * @LastEditTime: 2022-07-15 17:06:36
  * @Description: 数学问题
  * @FilePath: \leetcode\src\math.js
  * 可以输入预定的版权声明、个性签名、空行等
  */
+
+/** 3的幂
+ * 给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false;
+ * 整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3^x
+ * 
+输入：n = 27
+输出：true
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function (n) {
+    /** 执行用时： 156 ms , 在所有 JavaScript 提交中击败了 88.54% 的用户
+     * 内存消耗： 48.9 MB , 在所有 JavaScript 提交中击败了 99.65% 的用户 */
+    let count = 0;
+    while (Math.pow(3, count) < n) {
+        count++;
+    }
+    return Math.pow(3, count) === n;
+};
+
+/** 罗马数字转整数
+ * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M
+字符          数值
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+例如， 罗马数字 2 写做 II ，即为两个并列的 1 。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+给定一个罗马数字，将其转换成整数。
+
+输入: s = "IX"
+输出: 9
+
+输入: s = "IV"
+输出: 4
+
+输入: s = "LVIII"
+输出: 58
+解释: L = 50, V= 5, III = 3.
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    const typeMap = {
+        'I': 1,
+        'V': 5, // I 放在 V (5)的左边，表示 4
+        'X': 10, // I 放在 X (10)的左边，表示 9
+        'L': 50, // X 放在 L (50)左边，表示 40
+        'C': 100, // X 放在C (100) 的左边，表示 90
+        'D': 500,
+        'M': 1000
+    }
+    let count = 0;
+    let curStr = s;
+    // while(curStr.length) {
+    //     switch(curStr) {
+    //         case M
+    //     }
+    // }
+
+};
--- src/other.js
+++ src/other.js
@@ -1,7 +1,66 @@
 /*
  * @Author: jimouspeng
- * @LastEditTime: 2022-03-05 18:11:50
+ * @LastEditTime: 2022-07-15 17:39:19
  * @Description: 其他问题
  * @FilePath: \leetcode\src\other.js
  * 可以输入预定的版权声明、个性签名、空行等
  */
+
+/** 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效
+ * 
+ * 有效字符串需满足：
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+
+输入：s = "()[]{}"
+输出：true
+
+输入：s = "(]"
+输出：false
+
+输入：s = "{[]}"
+输出：true
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {};
+
+/** 缺失数字
+ * 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+ * 输入：nums = [3,0,1]
+ * 
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+nums 中的所有数字都 独一无二
+ * 
+输出：2
+解释：n = 3，因为有 3 个数字，所以所有的数字都在范围 [0,3] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+    /** 执行用时： 68 ms , 在所有 JavaScript 提交中击败了 73.49% 的用户
+     * 内存消耗： 43.3 MB , 在所有 JavaScript 提交中击败了 50.84% 的用户 */
+    let listLen = nums.length;
+    if (nums.length === 1 && nums[0] !== 1) {
+        return 1;
+    }
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < listLen; i++) {
+        if (nums[i] !== i) {
+            return i;
+        }
+    }
+    if (nums[listLen - 1] !== listLen) {
+        return listLen;
+    }
+};
+
+// console.log(missingNumber([3, 0, 1]));
+console.log(missingNumber([0, 1]));
