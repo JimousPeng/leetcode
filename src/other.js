5dfc6478a148d39823abb1e0bed93ce96da06ebe
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-07-18 11:39:33 +0800

    fix

--- src/other.js
+++ src/other.js
@@ -1,10 +1,34 @@
 /*
  * @Author: jimouspeng
- * @LastEditTime: 2022-07-15 17:39:19
+ * @LastEditTime: 2022-07-18 11:27:24
  * @Description: 其他问题
  * @FilePath: \leetcode\src\other.js
  * 可以输入预定的版权声明、个性签名、空行等
  */
+
+/** 杨辉三角
+ *在「杨辉三角」中，每个数是它左上方和右上方的数的和
+
+输入: numRows = 5
+输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+输入: numRows = 1
+输出: [[1]]
+ */
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+    if (numRows === 1) {
+        return [[1]];
+    }
+    let curList = [[1]]
+    while (numRows) {
+
+        numRows--;
+    }
+};
 
 /** 有效的括号
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效
@@ -26,7 +50,38 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {};
+var isValid = function (s) {
+    /** 执行用时： 68 ms , 在所有 JavaScript 提交中击败了 33.62% 的用户
+     * 内存消耗： 41.5 MB , 在所有 JavaScript 提交中击败了 27.21% 的用户 */
+    if (s.length < 2 || s.length % 2 !== 0) {
+        /** 先处理边界，有效提高效率 */
+        return false;
+    }
+    const hasMap = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+    };
+    const mapList = [];
+    for (let i = 0; i < s.length; i++) {
+        const key = s[i];
+        if (hasMap[key]) {
+            mapList.unshift(hasMap[key]);
+        } else {
+            if (mapList.length === 0) {
+                return false;
+            }
+            const curEle = mapList.shift();
+            if (s[i] !== curEle) {
+                return false;
+            }
+        }
+    }
+    return mapList.length === 0;
+};
+// isValid('()[]{}');
+// isValid('(]');
+isValid('([)]');
 
 /** 缺失数字
  * 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
