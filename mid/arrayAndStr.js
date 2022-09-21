b80819fc9b557bd9dc489c8ef4af5ad7c9a6ac09
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-09-21 14:41:47 +0800

    mid

--- mid/arrayAndStr.js
+++ mid/arrayAndStr.js
@@ -126,7 +126,7 @@ var setZeroes = function (matrix) {
 // );
 
 /** 字母异位词分组
- * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表
+ * 给你一个字符串数组，请你将[字母异位词]组合在一起。可以按任意顺序返回结果列表
  * 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次
  *
  * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
@@ -135,8 +135,22 @@ var setZeroes = function (matrix) {
  * 输入: strs = [""]
  * 输入: strs = [""]
  *
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ *
  * @param {string[]} strs
  * @return {string[][]}
+ * strs[i] 仅包含小写字母
+ * 0 <= strs[i].length <= 100
+ * 1 <= strs.length <= 104
  */
-var groupAnagrams = function (strs) {};
+var groupAnagrams = function (strs) {
+    if (strs.length === 1) {
+        return [[strs[0]]];
+    }
+    const strLen = strs.length;
+    const targetList = [];
+    let curStr = strs[0];
+    for (let i = 1; i < strLen; i++) {}
+};
 
