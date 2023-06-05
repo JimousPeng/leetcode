89414f6e606aac84a23118e9c5bca14b56163d51
jimouspeng <972523881@qq.com>
2023-06-05 10:41:16 +0800

    happy today

--- tree.js
+++ tree.js
@@ -6,6 +6,24 @@
  * }
  */
 
+/** 617. 合并二叉树
+ * 给你两棵二叉树： root1 和 root2; 需要将这两棵树合并成一棵新二叉树。
+ * 合并的规则是：如果两个节点重叠，那么将这两个节点的值相加作为合并后节点的新值；否则，不为 null 的节点将直接作为新二叉树的节点
+ * 合并过程必须从两个树的根节点开始
+ */
+var mergeTrees = function (root1, root2) {
+    function crossTree(root1, root2) {
+        if (root1 === null || root2 === null) {
+            return root1 || root2
+        }
+        let root = new TreeNode(root1.val + root2.val)
+        root.left = crossTree(root1.left, root2.left)
+        root.right = crossTree(root1.right, root2.right)
+        return root
+    }
+    return crossTree(root1, root2)
+}
+
 /**  572. 另一棵树的子树
  *   给你两棵二叉树 root 和 subRoot 。
  *   检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则，返回 false
