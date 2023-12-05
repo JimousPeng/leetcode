b7780f4bdef0f98eaa06ad38198604de1caac3b7
aiden <972523881@qq.com>
2023-12-05 22:46:55 +0800

    daily happy

--- tree.js
+++ tree.js
@@ -6,6 +6,45 @@
  * }
  */
 
+/** 671. 二叉树中第二小的节点
+ * 给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为 2 或 0。
+ * 如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个
+ * 更正式地说，即 root.val = min(root.left.val, root.right.val) 总成立
+ * 给出这样的一个二叉树，你需要输出所有节点中的 第二小的值   如果第二小的值不存在的话，输出 -1
+ * 输入：root = [2,2,5,null,null,5,7]  输出：5
+ * 解释：最小的值是 2 ，第二小的值是 5 。
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findSecondMinimumValue = function (root) {
+    let minest = []
+    function crossTree(root) {
+        if (root === null) return
+        if (!minest[0]) {
+            minest[0] = root.val
+        } else if (!minest[1]) {
+            let init = root.val
+            if (init < minest[0]) {
+                init = minest[0]
+                minest[0] = root.val
+            }
+            minest[1] = init
+        } else if (minest[0] === minest[1]) {
+            minest[1] = root.val
+        } else if (root.val > minest[0] && root.val < minest[1]) {
+            minest[1] = root.val
+        } else if (root.val < minest[0]) {
+            minest[1] = minest[0]
+            minest[0] = root.val
+        }
+        crossTree(root.left)
+        crossTree(root.right)
+    }
+    crossTree(root)
+    console.log(minest)
+    return minest[0] < minest[1] ? minest[1] : -1
+}
+
 /** 653. 两数之和 IV - 输入二叉搜索树
  *  给定一个二叉搜索树 root 和一个目标结果 k，如果二叉搜索树中存在两个元素且它们的和等于给定的目标结果，则返回 true
  *  输入: root = [5,3,6,2,4,null,7], k = 9  输出: true
