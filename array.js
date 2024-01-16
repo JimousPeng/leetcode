7cdd86e43635d227ae1c1fa5fa55ac9a5f650470
aiden <972523881@qq.com>
2024-01-16 16:43:18 +0800

    daily happy

--- array.js
+++ array.js
@@ -1,5 +1,48 @@
 /** 数组操作相关 */
 
+/** 219. 存在重复元素 II
+ *  给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。
+ *  如果存在，返回 true ；否则，返回 false
+ *
+ *  输入：nums = [1,2,3,1], k = 3  输出：true
+ *  输入：nums = [1,2,3,1,2,3], k = 2 输出：false
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+    // 找到相同的两个值，满足索引不同，且下标间隔 <= k
+
+    const numsMap = {}
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i]
+        // 因为下标是从0开始，所以要处理 numsMap 为0的情况
+        if ((numsMap[num] || numsMap[num] == 0) && i - numsMap[num] <= k) {
+            return true
+        } else {
+            numsMap[num] = i
+        }
+    }
+    return false
+
+    // 两个while循环太费时了，超出时间限制
+    // let left = 0
+    // let flag = false
+    // while (left < nums.length - 1) {
+    //     if (flag) return flag
+    //     let right = left + 1
+    //     while (right <= nums.length - 1) {
+    //         if (nums[right] === nums[left] && right - left <= k) {
+    //             flag = true
+    //             return flag
+    //         }
+    //         right++
+    //     }
+    //     left++
+    // }
+    // return false
+}
+
 /** 26. 删除有序数组中的重复项
  * 给你一个 非严格递增排列 -> 应该是升序数组，但是有重复项 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，
  * 返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数
--- link.js
+++ link.js
@@ -5,6 +5,43 @@
  * }
  */
 
+/** 203. 移除链表元素
+ *  给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点
+ *  输入：head = [1,2,6,3,4,5,6], val = 6  输出：[1,2,3,4,5]
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+    // 递归效率不如while循环好
+    if (head == null) return null
+
+    head.next = removeElements(head.next, val)
+    if (head.val === val) {
+        return head.next
+    } else {
+        return head
+    }
+
+    // let headNode = head
+    // let cur = head
+    // let prev = null
+    // while (cur) {
+    //     if (headNode.val === val) {
+    //         headNode = headNode.next
+    //     }
+    //     if (cur.val === val) {
+    //         if (prev) {
+    //             prev.next = cur.next
+    //         }
+    //     } else {
+    //         prev = cur
+    //     }
+    //     cur = cur.next
+    // }
+    // return headNode
+}
+
 // 83. 删除排序链表中的重复元素
 /**
  * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表
@@ -15,13 +52,13 @@ var deleteDuplicates = function (head) {
     if (head == null || head.next == null) {
         return head
     }
-    let res = head;
+    let res = head
     while (head) {
-        if(head.next === null) break;
-        if(head.val === head.next.val) {
+        if (head.next === null) break
+        if (head.val === head.next.val) {
             head.next = head.next.next
         } else {
-            head = head.next;
+            head = head.next
         }
     }
     return res
