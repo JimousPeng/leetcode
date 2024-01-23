fe5b44e229e92a616617f4c504757c98b95254c1
aiden <972523881@qq.com>
2024-01-23 19:32:49 +0800

    code

--- functional.js
+++ functional.js
@@ -1,5 +1,39 @@
 /** 功能性题目，以解决问题为出发点 */
 
+/** 485. 最大连续 1 的个数
+ * 给定一个二进制数组 nums ， 计算其中最大连续 1 的个数
+ * 输入：nums = [1,1,0,1,1,1] 输出：3
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+    // 思路，既然是二进制数，那么先将0切割，然后逐个对比长度
+    const str = nums.join('')
+    const numsList = str.split('0')
+    const numLen = numsList.length
+    let max = 0
+    for (let i = 0; i < numLen; i++) {
+        const len = numsList[i].length
+        max = Math.max(max, len)
+    }
+    return max
+
+    // 正常遍历, 性能更好，不需要数组转换
+    let maxCount = 0,
+        count = 0
+    const n = nums.length
+    for (let i = 0; i < n; i++) {
+        if (nums[i] === 1) {
+            count++
+        } else {
+            maxCount = Math.max(maxCount, count)
+            count = 0
+        }
+    }
+    maxCount = Math.max(maxCount, count)
+    return maxCount
+}
+
 /** 482. 密钥格式化
  *  给定一个许可密钥字符串 s，仅由字母、数字字符和破折号组成。字符串由 n 个破折号分成 n + 1 组。你也会得到一个整数 k
  *
