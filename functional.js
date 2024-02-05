03e9f175afdd0beb40aed78838cf826080ac3abe
aiden <972523881@qq.com>
2024-02-05 15:26:24 +0800

    daily happy

--- functional.js
+++ functional.js
@@ -13,25 +13,6 @@
  * @return {number[][]}
  */
 var combinationSum = function (candidates, target) {
-    const ans = []
-    const dfs = (target, combine, idx) => {
-        if (idx === candidates.length) {
-            return
-        }
-        if (target === 0) {
-            ans.push(combine)
-            return
-        }
-        // 直接跳过
-        dfs(target, combine, idx + 1)
-        // 选择当前数
-        if (target - candidates[idx] >= 0) {
-            dfs(target - candidates[idx], [...combine, candidates[idx]], idx)
-        }
-    }
-
-    dfs(target, [], 0)
-    return ans
 }
 
 /** 34. 在排序数组中查找元素的第一个和最后一个位置
