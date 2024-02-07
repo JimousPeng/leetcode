03368ed6da76b74f2c6c3dad4c43f8d492591674
aiden <972523881@qq.com>
2024-02-07 17:11:07 +0800

    daily happy

--- functional.js
+++ functional.js
@@ -13,27 +13,26 @@
  * @return {number[][]}
  */
 var combinationSum = function (candidates, target) {
-    // 回溯算法
-    if (candidates[0] > target) return []
-
+    // 回溯算法 以 [2,3,6,7] 为例
+    //
     const ans = []
-    const dfs = (target, combine, idx) => {
-        if (idx === candidates.length) {
-            return
-        }
+
+    function dfs(target, combine, idx) {
+        if (idx === candidates.length) return
         if (target === 0) {
+            // target 为 0， 即得到组合综合 = target
             ans.push(combine)
             return
         }
-        // 直接跳过
-        dfs(target, combine, idx + 1)
-        // 选择当前数
+        dfs(target, combine, idx + 1) // 递归处理下一个数
         if (target - candidates[idx] >= 0) {
+            // 当前数处理，每个数字可以被无限制重复选取，所以这里idx可以不变
             dfs(target - candidates[idx], [...combine, candidates[idx]], idx)
         }
     }
 
     dfs(target, [], 0)
+
     return ans
 }
 
