3717595eb35606513b0e8474e1d93f5fc8c8cc1c
aiden <972523881@qq.com>
2024-02-22 16:30:13 +0800

    二叉树遍历方式回顾

--- functional.js
+++ functional.js
@@ -21,6 +21,39 @@ var maxProduct = function (nums) {
     }
 }
 
+/** 79. 单词搜索
+ * 给定一个 m x n 二维字符网格 board 和一个字符串单词 word
+ * 如果 word 存在于网格中，返回 true ；否则，返回 false
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+ * 输出：true
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+    /** 先构建字符坐标 */
+    const strMap = {}
+    const rowLen = board.length
+    for (let i = 0; i < rowLen; i++) {
+        const row = board[i]
+        const rowLen = row.length
+        for (let j = 0; j < rowLen; j++) {
+            const str = row[j]
+            if (strMap[str]) {
+                strMap[str].push([i, j])
+            } else {
+                strMap = [[i, j]]
+            }
+        }
+    }
+
+    const wordLen = word.length
+    function trackingBack() {
+        for (let i = 0; i < wordLen; i++) {}
+    }
+}
+
 /** 64. 最小路径和
  * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
  * 说明：每次只能向下或者向右移动一步。
