a9c648bc28c0b18b149d02336ebee3f32bad5060
aiden <972523881@qq.com>
2024-03-14 13:48:10 +0800

    daily happy

--- functional.js
+++ functional.js
@@ -1,5 +1,37 @@
 /** 功能性题目，以解决问题为出发点 */
 
+/** 面试题 01.04. 回文排列
+ * 给定一个字符串，编写一个函数判定其是否为某个回文串的排列之一。
+ * 回文串是指正反两个方向都一样的单词或短语。排列是指字母的重新排列
+ * 回文串不一定是字典当中的单词。
+ * 输入："tactcoa" 输出：true（排列有"tacocat"、"atcocta"，等等）
+ * 'code' -> false
+ * @param {string} s
+ * @return {boolean}
+ */
+var canPermutePalindrome = function (s) {
+    // 不需要回溯，只需要判断字符串是否最多只有一个单字母，其他都是对数存在即可
+
+    if (s.length < 2) return true
+    const strLen = s.length
+    const strMap = {}
+    for (let i = 0; i < strLen; i++) {
+        const str = s[i]
+        if (strMap[str]) {
+            strMap[str]++
+        } else {
+            strMap[str] = 1
+        }
+    }
+    let flag = 0
+    Object.keys(strMap).forEach((str) => {
+        if (strMap[str] % 2 === 1) {
+            flag++
+        }
+    })
+    return flag < 2
+}
+
 /** 面试题 01.06. 字符串压缩
  * 利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。
  * 比如，字符串aabcccccaaa会变为a2b1c5a3。
