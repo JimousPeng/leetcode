8b24829b327e3bd222a8967f370c2231eef51d0a
aiden <972523881@qq.com>
2024-04-19 20:18:29 +0800

    daily happy

--- tree.js
+++ tree.js
@@ -6,6 +6,44 @@
  * }
  */
 
+/** 面试题 04.10. 检查子树
+ * 检查子树。你有两棵非常大的二叉树：T1，有几万个节点；T2，有几万个节点。
+ * 设计一个算法，判断 T2 是否为 T1 的子树
+ * 如果 T1 有这么一个节点 n，其子树与 T2 一模一样，则 T2 为 T1 的子树，也就是说，从节点 n 处把树砍断，得到的树与 T2 完全相同
+ *  输入：t1 = [1, 2, 3], t2 = [2]  输出：true
+ *  输入：t1 = [1, null, 2, 4], t2 = [3, 2]  输出：false
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {boolean}
+ * 判断 T2 是否为 T1 的子树
+ */
+var checkSubTree = function (t1, t2) {
+    if (t2 === null) return true
+
+    let flag = false
+
+    function dfs(root) {
+        if(root === null) return
+        if (flag) return
+        flag = isSameTree(root, t2)
+        dfs(root.left)
+        dfs(root.right)
+    }
+
+    function isSameTree(root1, root2) {
+        if (root1 === null && root2 !== null) return false
+        if (root1 !== null && root2 === null) return false
+        if (root1 === null && root2 === null) return true
+        let left = isSameTree(root1.left, root2.left)
+        let right = isSameTree(root1.right, root2.right)
+        return root1.val === root2.val && left && right
+    }
+
+    dfs(t1)
+
+    return falg
+}
+
 /** 面试题 04.12. 求和路径
  * 给定一棵二叉树，其中每个节点都含有一个整数数值(该值或正或负)
  * 设计一个算法，打印节点数值总和等于某个给定值的所有路径的数量
