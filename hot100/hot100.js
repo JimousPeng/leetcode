868e87356afc15462ecb01967622fab0565ec5d2
aiden <972523881@qq.com>
2024-08-05 22:27:59 +0800

    init

--- hot100/hot100.js
+++ hot100/hot100.js
@@ -351,3 +351,17 @@ var moveZeroes = function (nums) {
     }
     return setPos()
 }
+
+/**
+ * 11. 盛最多水的容器
+ * 给定一个长度为 n 的整数数组 height,有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i])
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水
+ * 返回容器可以储存的最大水量; 说明：你不能倾斜容器
+ *
+ * 输入：[1,8,6,2,5,4,8,3,7]  输出：49; 容纳的最大宽高为 7 x 7 = 49 参考 './question_11.jpg'
+ *
+ * 输入：height = [1,1] 输出：1
+ * @param {number[]} height  2 <= height.length <= 10^5    0 <= height[i] <= 10^4
+ * @return {number}
+ */
+var maxArea = function (height) {}
