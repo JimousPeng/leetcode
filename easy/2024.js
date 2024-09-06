d0876ef2873c3585c6184615d09ce14b875d124e
aiden <972523881@qq.com>
2024-09-06 11:23:57 +0800

    daily happy

--- easy/2024.js
+++ easy/2024.js
@@ -312,3 +312,37 @@ var isToeplitzMatrix = function (matrix) {
         return true
     }
 }
+
+/**
+ * 771. 宝石与石头
+ * @param {string} jewels 1 <= jewels.length, stones.length <= 50
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+    /**
+     * 给你一个字符串 jewels 代表石头中宝石的类型
+     * 另有一个字符串 stones 代表你拥有的石头。 
+     * stones 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+     * 字母区分大小写，因此 "a" 和 "A" 是不同类型的石头
+示例 1：
+输入：jewels = "aA", stones = "aAAbbbb"
+输出：3
+示例 2：
+输入：jewels = "z", stones = "ZZ"
+输出：0
+     */
+    let res = 0
+    const jewelsSet = new Set()
+    const jewelsLen = jewels.length
+    const stonesLen = stones.length
+    for (let i = 0; i < jewelsLen; i++) {
+        jewelsSet.add(jewels[i])
+    }
+    for (let i = 0; i < stonesLen; i++) {
+        if (jewelsSet.has(stones[i])) {
+            res++
+        }
+    }
+    return res
+}
--- hot100/hot100.js
+++ hot100/hot100.js
@@ -1447,3 +1447,101 @@ var spiralOrder = function (matrix) {
         return res
     }
 }
+
+/**
+ * 48. 旋转图像 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像
+示例 1：
+输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+1  2  3      7  4  1
+4  5  6  ->  8  5  2
+7  8  9      9  6  3
+
+如果要原地修改 -> 先上下(水平)翻转，再对角线
+
+1  2  3     7  8  9     7  4  1
+4  5  6  -> 4  5  6  -> 8  5  2
+7  8  9     1  2  3     9  6  3
+
+输出：[[7,4,1],[8,5,2],[9,6,3]]
+
+示例 2：
+输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+5  1  9  11
+2  4  8  10
+13 3  6  7
+15 14 12 16
+输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ * @param {number[][]} matrix  n == matrix.length == matrix[i].length  1 <= n <= 20
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+    /**
+     * 顺时针旋转90度，也就是意味着横的变竖的
+     * 先试试不原地旋转怎么解决
+     */
+    function useAnother() {
+        const newMatrix = []
+        // n x n 其实 rowLen === colLen
+        const rowLen = matrix.length
+        const colLen = matrix[0].length
+        let c = 0
+        while (c < colLen) {
+            const rowArr = []
+            let r = rowLen - 1
+            while (r >= 0) {
+                rowArr.push(matrix[r][c])
+                r--
+            }
+            newMatrix.push(rowArr)
+            c++
+        }
+        for (let i = 0; i < rowLen; i++) {
+            for (let j = 0; j < colLen; j++) {
+                matrix[i][j] = newMatrix[i][j]
+            }
+        }
+    }
+
+    /** 翻转两次 */
+    function reverse() {
+        const rowLen = matrix.length
+        const colLen = matrix[0].length
+
+        // 第一次水平翻转
+        for (let i = 0; i < Math.floor(rowLen / 2); i++) {
+            for (let j = 0; j < colLen; j++) {
+                const temp = matrix[i][j]
+                matrix[i][j] = matrix[rowLen - 1 - i][j]
+                matrix[rowLen - 1 - i][j] = temp
+            }
+        }
+
+        // 第二次对角线翻转
+        for (let i = 0; i < colLen; i++) {
+            for (let j = i + 1; j < rowLen; j++) {
+                const temp = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = temp
+            }
+        }
+
+        // 官方题解
+        // var rotate = function (matrix) {
+        //     const n = matrix.length
+        //     // 水平翻转
+        //     for (let i = 0; i < Math.floor(n / 2); i++) {
+        //         for (let j = 0; j < n; j++) {
+        //             ;[matrix[i][j], matrix[n - i - 1][j]] = [matrix[n - i - 1][j], matrix[i][j]]
+        //         }
+        //     }
+        //     // 主对角线翻转
+        //     for (let i = 0; i < n; i++) {
+        //         for (let j = 0; j < i; j++) {
+        //             ;[matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+        //         }
+        //     }
+        // }
+    }
+}
